63b12ff50d28622f0a420d1992ca9f9a367080d7
t-east <azmtomya.a127@gmail.com>
2022-03-19 04:57:34 +0000

    baseコンポーネント設置

---
+++ user/components/BaseHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="h-14 py-2 px-4 flex justify-between items-center text-white">
+    <div @click="home()">
+      aaa
+    </div>
+    <slot name="right" />
+  </header>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  components: {
+  },
+  methods: {
+    home(): void {
+    //   // @ts-ignore
+    //   if (this.$store.$auth.loggedIn && this.$auth.user.user_type === 'consumer') {
+    //     this.$router.push({ path: '/home' });
+    //     // @ts-ignore
+    //   } else if (this.$store.$auth.loggedIn && this.$auth.user.user_type === 'stylist') {
+    //     this.$router.push({ path: '/s/home' });
+    //   } else {
+    //     this.$router.push({ path: '/' });
+    //   }
+    },
+  },
+});
+</script>
---
+++ user/components/BaseModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="fixed inset-0 h-screen flex items-center justify-center z-50"
+    :class="$props.bgColor"
+  >
+    <div class="sm:w-80 w-5/6 max-w-lg modal-height bg-white text-black rounded-2xl px-8 my-12 relative">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    bgColor: { type: String, default: 'bg-green-900' },
+  },
+});
+</script>
+
+<style scoped>
+.modal-height {
+  height: 85%
+}
+</style>
