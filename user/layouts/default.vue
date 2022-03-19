19146b1066df74db58d1b7d31199f8628b87a49f
t-east <azmtomya.a127@gmail.com>
2022-03-19 04:58:46 +0000

    ページinit

--- user/layouts/default.vue
+++ user/layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="bg-gray-300">
-    <nuxt />
+  <div class="">
+    <BaseHeader class="bg-green-900">
+      <template #right>
+        <div class="flex items-center space-x-2 cursor-pointer" @click="$emit('open-menu')">
+          <span class="inline-block align-middle text-lg py-2">
+            asd
+          </span>
+        </div>
+      </template>
+    </BaseHeader>
+    <div class="">
+      <Nuxt/>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import BaseHeader from '~/components/BaseHeader.vue';
+
+export default Vue.extend({
+  components: {
+    BaseHeader
+  }
+})
+</script>
--- user/pages/index.vue
+++ user/pages/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="container">
-    aaaa
+    <RegisterModal v-if="isRegisterModal"/>
+    <UploadModal/>
+    <AtomFileCard />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import RegisterModal from '~/components/organisms/RegisterModal.vue';
+import UploadModal from '~/components/organisms/UploadModal.vue';
+import AtomFileCard from '~/components/atoms/AtomFileCard.vue';
+
 export default Vue.extend({
   components: {
+    RegisterModal,
+    AtomFileCard,
+    UploadModal
+  },
+  data() {
+    return {
+      isRegisterModal: false
+    }
   }
 })
 </script>
