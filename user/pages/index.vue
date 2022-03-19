aa3d9fd36cf3b7a473cdf6dda4acf8d46de3b722
t-east <azmtomya.a127@gmail.com>
2022-03-19 02:09:07 +0000

    init プロジェクト

---
+++ user/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    aaaa
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+  }
+})
+</script>
